705b993cfc70ad386cba1b55e28a8e8180a0dbe0
Rajaganapathy M <rajaganaa@gmail.com>
2026-06-06 14:05:38 +0530

    fix: CORS

--- backend/main.py
+++ backend/main.py
@@ -55,7 +55,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rajaganaa.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
